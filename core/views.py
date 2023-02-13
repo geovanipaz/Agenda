3cc59fefa6fb053db747e5d7ab5710ac361b3e3f
geovani <geovanipaz7@gmail.com>
2023-02-13 20:49:21 -0300

    implementação lista de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+from core.models import Evento
 # Create your views here.
+
+def lista_eventos(request):
+    #usuario = request.user
+    eventos  = Evento.objects.all()
+    resposta = {'eventos':eventos}
+    return render(request,'agenda.html', resposta)
